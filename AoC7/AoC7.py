d9fc4484c638c573f383e409a57e5062298e24f3
SebastianvonAllmen <sebastian.v.allmen@gmail.com>
2021-12-07 11:03:33 +0100

    Finished part 2 of ex7

--- AoC7/AoC7.py
+++ AoC7/AoC7.py
@@ -1,25 +1,52 @@
 from lib import *
 import re
+import math
 
-def calculatefuel (startingpos, endpos):
+
+def calculatefuel(startingpos, endpos):
     return abs(startingpos - endpos)
 
-def middle_element(lst):
-  if len(lst) % 2 != 0:
-    return lst[int(len(lst) / 2)]
-  else:
-    return (((lst[int(len(lst) / 2)]) + (lst[int(len(lst) / 2) - 1])) / 2)
 
+def middle_element(lst):
+    if len(lst) % 2 != 0:
+        return lst[int(len(lst) / 2)]
+    else:
+        return (((lst[int(len(lst) / 2)]) + (lst[int(len(lst) / 2) - 1])) / 2)
+
+
+def mean(list):
+    mean = sum(list) / len(list)
+    return math.floor(mean), math.ceil(mean)
+
+
+def gausSum(n):
+    return ((n ** 2) + n) // 2
+
+
+# File I/O
 input = input_as_string("input7.txt")
 numbers = list(map(int, re.findall(r'(\d+)', input)))
 
-#find middle element of list
+# Part 1
+# find middle element of list
 numbers.sort()
 target = int(middle_element(numbers))
 
 fuel = 0
-#Calculate all the fuel needed
+# Calculate all the fuel needed
 for i in range(len(numbers)):
     fuel += calculatefuel(numbers[i], target)
 
 print(fuel)
+
+# Part 2
+fuelfloor = 0
+fuelceil = 0
+
+meanfloor, meanceil = mean(numbers)
+
+for i in range(len(numbers)):
+    fuelfloor += gausSum(calculatefuel(numbers[i], meanfloor))
+    fuelceil += gausSum(calculatefuel(numbers[i], meanceil))
+
+print(min(fuelceil, fuelfloor))
---
+++ AoC7/git.py
@@ -0,0 +1,37 @@
+import math
+
+def main():
+    with open("input7.txt", "r") as f:
+        line = f.read()
+        # print(part1(line))
+        print(part2(line))
+
+def median(lst):
+    lst.sort()
+    if len(lst) % 2 == 0:
+        return (lst[len(lst) // 2] + lst[(len(lst) - 1) // 2]) / 2
+    else:
+        return lst[len(lst) // 2]
+
+def part1(line):
+    lineList = [int(item) for item in line.split(',')]
+    median_value = int(median(lineList))
+    return sum([abs(foo - median_value) for foo in lineList])
+
+def part2(line):
+    lineList = [int(item) for item in line.split(',')]
+    mean0, mean1 = get_means(lineList)
+    return min(
+        sum(t(abs(item - mean0)) for item in lineList),
+        sum(t(abs(item - mean1)) for item in lineList),
+    )
+
+def get_means(lineList):
+    mean = sum(lineList) / len(lineList)
+    return math.floor(mean), math.ceil(mean)
+
+def t(n):
+    return n * (n + 1) // 2
+
+if __name__ == "__main__":
+    main()
